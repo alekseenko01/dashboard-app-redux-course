03d8cec6e7d704abacf6cddfb57e5d0a11e98000
Aleksei <alekseenko.json@gmail.com>
2023-05-02 23:52:21 +0300

    first commit

---
+++ src/store/root-reducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+
+import {positionReducer} from "./positions/positions-reducer"
+import { filterReducer } from "./filters/filter-reducers";
+
+export const rootReducer = combineReducers({
+  positions: positionReducer,
+  filters: filterReducer
+})
